444c10b8f469c34e4395721f06f5ea72684c0b2d
Chris Abrams <mail@chrisabrams.com>
2016-02-02 21:30:25 -0500

    Associate user to organization

--- models/user.js
+++ models/user.js
@@ -8,13 +8,93 @@ class UserModel extends Model {
     super(options = {})
   }
 
+  addToOrganization(id, orgId, cb) {
+
+    pkg.dateCreated = r.now()
+    pkg.dateUpdate  = r.now()
+
+    async.parallel([
+      this.addOrganizationToUser.bind(this, id, orgId),
+      this.addToOrganizationTable.bind(this, id, orgId)
+    ], function addToOrganizationCallback(err, res) {
+
+      if(err) {
+        logger.error('Could not add user to organization', err)
+        return cb(err)
+      }
+
+      return cb(null, res[0])
+
+    })
+
+  }
+
+  addOrganizationToUser(id, orgId, cb) {
+
+    this.query((r) => r.db(this.config.db.name).table(this.name).get(id).update({
+      organizations: r.row('organizations').append(orgId)
+    }, {
+      durability: 'hard',
+      returnChanges: true
+    }), function addOrganizationToUserCallback(err, res) {
+      if(err) {
+        logger.error('Could not add organization to user', err)
+        return cb(err)
+      }
+
+      cb(null, res.changes[0].new_val)
+    })
+
+  }
+
+  addToOrganizationTable(id, orgId, cb) {
+
+    this.query((r) => r.db(this.config.db.name).table('organization_has_users').insert({
+      dateCreated: r.now(),
+      organizationId: orgId,
+      userId: id
+    }, {
+      durability: 'hard',
+      returnChanges: true
+    }), function addToOrganizationTableCallback(err, res) {
+      if(err) {
+        logger.error('Could not create', err)
+        return cb(err)
+      }
+
+      cb(null, res.changes[0].new_val)
+    })
+
+  }
+
   create(pkg, cb) {
-    pkg._status = 'active'
-    super._create.call(this, pkg, cb)
+    super._create.apply(this, arguments)
   }
 
   get() {
     super._get.apply(this, arguments)
+  }
+
+  getByEmail(email, cb) {
+
+    this.query((r) => r.db(this.config.db.name).table(this.table).filter({email}).limit(1), function getCallback(err, res) {
+      if(err) {
+        return cb(err)
+      }
+
+      if(res.length == 0) {
+        return cb({
+          message: 'No users found',
+          note: `Attempted to look up user user by email ${email}`,
+          resource: 'UserModel'
+        })
+      }
+
+      var user = res[0]
+
+      cb(null, user)
+    })
+
   }
 
   getByUsername(username, cb) {
--- services/user.js
+++ services/user.js
@@ -19,6 +19,9 @@ class UserService {
         tokenService    = new TokenService(),
         userModel       = new UserModel()
 
+    /*
+    TODO: clean up this query, it could be optimized
+    */
     passwordService.issue(pkg.password, function UserService_createIssueCallback(err, hash) {
 
       if(err) {
@@ -30,30 +33,44 @@ class UserService {
         })
       }
 
-      delete pkg.password // dont want to store actual password
+      if(pkg.password) delete pkg.password // dont want to store actual password
+      if(pkg.organizationId) delete pkg.organizationId
 
-      pkg.hash   = hash
+      pkg.hash = hash
+      pkg.organizations = pkg.organizations || []
 
       async.parallel([
-        userModel.create.bind(userModel, pkg),
-        tokenService.issue.bind(_this, hash)
+        userModel.create.bind(userModel, pkg)
       ], function UserService_createParallelCallback(err, res) {
+
+        if(err) {
+          logger.error(err)
+          return cb({
+            raw: err,
+            resource: 'UserModel'
+          })
+        }
 
         var user = res[0]
         var sid  = shortidService.issue(user.id)
 
-        _this.update(user.id, {sid}, function(err2, res2) {
+        async.parallel([
+          tokenService.issue.bind(tokenService, user.id),
+          userModel.addToOrganizationTable.bind(userModel, user.id, o.organizations[0]),
+          _this.update.bind(_this, user.id, {sid})
+        ], function UserService_createParallelCallback2(err2, res2) {
 
-          if(err) {
-            logger.error(err)
+          if(err2) {
+            logger.error(err2)
             return cb({
-              raw: err,
+              raw: err2,
               resource: 'UserModel'
             })
           }
 
-          user       = res2
-          user.token = res[1]
+          user               = res2[2]
+          user.organizations = res2[1].organizations
+          user.token         = res2[0]
 
           cb(null, user)
 
