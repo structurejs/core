87b8f4cb45c28b582dcffc0a22c3ce620a36a299
Chris Abrams <mail@chrisabrams.com>
2016-02-10 12:51:52 -0600

    Remove .only on test

--- test/integration/boot.js
+++ test/integration/boot.js
@@ -2,7 +2,7 @@ import Boot from '../../models/boot'
 
 describe('Integration: Models: Boot', function() {
 
-  it.only('should create tables on boot', function(done) {
+  it('should create tables on boot', function(done) {
 
     var boot = new Boot()
 
